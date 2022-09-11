186e10ade0bfcec9d04530e1aba92d109990675f
masterdpro <youri.d2.bel@gmail.com>
2022-09-11 14:14:16 -0400

    baguette

---
+++ commands/baguette.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { getMemberInventory, removeItem } = require("../utils/utilities");
+const shop = require("../utils/shop");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("baguette")
+    .setDescription("utilise ta baguette"),
+  async execute(interaction) {
+    const memberInventory = await getMemberInventory(interaction.member);
+
+    const itemName = ("Baguette");
+    const item = shop[itemName];
+    if (!memberInventory.includes(itemName)) return interaction.reply({content:"vous n'avez pas l'item en question dans votre inventaire (</inventory:1009850854213435423>)", ephemeral:true});
+    removeItem(interaction.member, item);
+    return interaction.reply(` oui oui baguette !`)
+  },
+};
