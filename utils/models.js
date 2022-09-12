ef24d871aa7b37b9cfe9685f0e7d7c72a6c96a72
masterdpro <youri.d2.bel@gmail.com>
2022-09-12 19:29:09 -0400

    skdfj

--- utils/models.js
+++ utils/models.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const yesterday = new Date();
 yesterday.setDate(yesterday.getDate() - 1);
 const hourDelay = new Date();
-hourDelay.setHours(hourDelay.getHours());
+hourDelay.setHours(hourDelay.getHours() - 1);
 // let author = await mongoose.fetch(`work_${message.guild.id}_${user.id}`)
 
 const memberShema = new mongoose.Schema({
