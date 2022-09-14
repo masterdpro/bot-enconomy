1e11276d982f44c3a02caf9d54b29025249ab325
masterdpro <youri.d2.bel@gmail.com>
2022-09-14 18:45:27 -0400

    sdjf

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -6,15 +6,14 @@ module.exports = {
 
     if (!command) return;
 
-    try {
-      await command.execute(interaction, client);
-    } catch (error) {
-      console.error(error);
-      await interaction.reply({ content: "erreur durant le procedée de la commande", ephemeral: true });
-    }} else if (interaction.isButton()){
+    
+    await command.execute(interaction, client);
+
+    } else if (interaction.isButton())
+    {
       const btn = client.buttons.get(interaction.customId);
         if (!btn) return interaction.reply('se boutton existe pas');
-        btn.runInteraction(client, interaction);
+        btn.execute(client, interaction);
     }
   },
-};
+}
