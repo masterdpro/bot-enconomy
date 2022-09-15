e52e9b34d85ef8e8fb45ed1be14ba6f79ea6f653
masterdpro <youri.d2.bel@gmail.com>
2022-09-15 17:26:20 -0400

    sdkfj

--- commands/inventory.js
+++ commands/inventory.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
 const { getMemberInventory } = require("../utils/utilities");
 
 module.exports = {
@@ -10,13 +11,24 @@ module.exports = {
   async execute(interaction) {
     const memberInventory = await getMemberInventory(interaction.member);
     const omember = interaction.options.getMember("user");
+
+    
     if (omember) {
-      const memberInventory = await getMemberInventory(omember);
-      interaction.reply(`l'inventaire de ${omember.tag} \n \`${memberInventory.join("\n")}\``);
+      const omemberInventory = await getMemberInventory(omember);
+      if (memberInventory == "") await interaction.reply("ton invetaire est vide :/");
+      const oembed = new MessageEmbed()
+      .setAuthor({ name: `L'inventaire de ${omember}`, iconURL: interaction.guild.iconURL() })
+      .setColor("RANDOM")
+      .addFIeld(`l'inventaire de ${omember} \n \`${omemberInventory.join("\n")}\``)
+      interaction.reply({ embeds: [oembed] });
     }
     if (omember == "") return
     else if (memberInventory == "") await interaction.reply("ton invetaire est vide :/");
-    return interaction.reply(`voici ton inventaire \n \`${memberInventory.join("\n")}\``);
+    const embed = new MessageEmbed()
+      .setAuthor({ name: `L'inventaire de ${interaction.author.name}`, iconURL: interaction.guild.iconURL() })
+      .setColor("RANDOM")
+      .addFIeld(`l'inventaire de ${interaction.author.name} \n \`${memberInventory.join("\n")}\``)
+    return interaction.reply({ embeds: [embed] });
   },
 };
 
