5507e26237c8eafc854c1f0b6a3dbbe374b34f56
masterdpro <youri.d2.bel@gmail.com>
2022-09-17 10:43:45 -0400

    kaskdas

--- commands/work.js
+++ commands/work.js
@@ -1,6 +1,13 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { work, addItem} = require("../utils/utilities");
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("work")
@@ -8,7 +15,7 @@ module.exports = {
   async execute(interaction) {
     //generate a random number between 0 ans 100
     const random = Math.floor(Math.random() * 101);
-    const randomc = Math.random() * (100 - 200) + 100;
+    const randomc = getRandomIntInclusive(100, 200)
     if (random == 100) {
       await interaction.reply({ content: "BRAVO, tu as une 1% de chance de tombé sur se message, tu gagne 1000 coins et tu remporte une **Lucky_sword**!", ephemeral: true });
       await work(interaction.member, 1000);
