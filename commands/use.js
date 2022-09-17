40a4068c07e9b58d90222818f6148ce5acd2a74f
masterdpro <youri.d2.bel@gmail.com>
2022-09-17 17:18:51 -0400

    dfkjsd

--- commands/use.js
+++ commands/use.js
@@ -17,8 +17,8 @@ module.exports = {
     const item = shop[itemName];
     if (!memberInventory.includes(itemName)) return interaction.reply("vous n'avez pas l'item en question dans votre inventaire (</inventory:1009850854213435423>)");
     if (item.sanction == 0) return interaction.reply(`${item.usage}`)
-    if (item.sanction > 0 && member == "") return interaction.reply(" pour utiliser litem tu dois cibler une personne")
-    if (item.sanction > 0 && member != "") {
+    if (item.sanction > 0 && member == " ") return interaction.reply(" pour utiliser litem tu dois cibler une personne")
+    if (item.sanction > 0 && member != " ") {
       removeItem(interaction.member, item);
       return interaction.reply(` ${member} ${item.usage}!`),
       await member.timeout(item.sanction, item.usage);
