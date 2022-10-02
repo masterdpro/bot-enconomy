6972f7675bbed8b09e1414c593cc841595cf061b
masterdpro <youri.d2.bel@gmail.com>
2022-10-02 19:16:17 -0400

    sdkjf

--- commands/fight.js
+++ commands/fight.js
@@ -15,9 +15,8 @@ module.exports = {
     const member = interaction.options.getMember("user");
     const memberInventory = await getMemberInventory(interaction.member);
     const itemName = capitalizeFirstLetter(interaction.options.getString("item"));
-    const item = shop[itemName];
-    const itemS = shop[itemSanction]
+    const item = shop[item]
     if (!memberInventory.includes(itemName)) return interaction.reply("vous n'avez pas l'item en question dans votre inventaire (</inventory:1009850854213435423>)");
-    return interaction.reply(await hpup(member, itemS));
+    return interaction.reply(await hpup(member, item.sanction));
   },
 };
