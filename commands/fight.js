8b339b16f3403f3154074dc2060a253ef898bfd9
masterdpro <youri.d2.bel@gmail.com>
2022-10-02 18:57:41 -0400

    ajout hp #3.6

--- commands/fight.js
+++ commands/fight.js
@@ -1,18 +1,22 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { give } = require("../utils/utilities");
+const { hpup } = require("../utils/utilities");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("fight")
     .setDescription("Affronté un membre")
-    .addNumberOption(option =>
-      option.setName("num").setDescription("entré le montant donné").setRequired(true).setMinValue(1),
+    .addStringOption(option =>
+      option.setName("item").setDescription("entré le montant donné").setRequired(true).setMinValue(1),
     )
     .addUserOption(option => option.setName("user")
       .setDescription("utilisateur a qui vous voulez donner").setRequired(true)),
   async execute(interaction) {
-    const number = interaction.options.getNumber("num");
-    const member = interaction.options.getMember("user", true);
-    return interaction.reply(await give(interaction.member, member, number));
+    const member = interaction.options.getMember("user");
+    const memberInventory = await getMemberInventory(interaction.member);
+    const itemName = capitalizeFirstLetter(interaction.options.getString("item"));
+    const item = shop[itemName];
+    const itemS = shop[itemSanction]
+    if (!memberInventory.includes(itemName)) return interaction.reply("vous n'avez pas l'item en question dans votre inventaire (</inventory:1009850854213435423>)");
+    return interaction.reply(await hpup(member, itemS));
   },
 };
